1c64e6ef8fd3ebe4c7c84bc43df24a83c8a344e3
Elias-Ruiz-Rodriguez <150531419+Elias-Ruiz-Rodriguez@users.noreply.github.com>
2024-10-31 19:24:38 -0300

    agregue las tablas y atributos

--- ruize/proyecto_brunette/admin.py
+++ ruize/proyecto_brunette/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Empleados, Login, Caja, Proveedores, Compras, Productos, DetalleCompra, Clientes, Ventas, DetalleVenta, PagoImpuestos, Impuestos, ProvProductos, Pedidos
 
-# Register your models here.
+admin.site.register(Empleados)
+admin.site.register(Login)
+admin.site.register(Caja)
+admin.site.register(Proveedores)
+admin.site.register(Compras)
+admin.site.register(Productos)
+admin.site.register(DetalleCompra)
+admin.site.register(Clientes)
+admin.site.register(Ventas)
+admin.site.register(DetalleVenta)
+admin.site.register(PagoImpuestos)
+admin.site.register(Impuestos)
+admin.site.register(ProvProductos)
+admin.site.register(Pedidos)
